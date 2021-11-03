25c876acef4e8baf6e65b6954feb31c92e9a90e2
xuwentingtingting <958349680@qq.com>
2021-11-03 17:04:59 +0800

    新增知识模块

---
+++ knowledgedisplay/src/main/java/com/example/knowledgedisplay/KnowledgeDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.knowledgedisplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KnowledgeDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knowledge_display);
+    }
+}
