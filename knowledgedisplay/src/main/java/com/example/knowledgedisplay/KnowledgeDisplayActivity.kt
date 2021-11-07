dc3985d254f7ceb2018600dad1c4a1c380c41a82
xuwentingtingting <958349680@qq.com>
2021-11-07 14:02:38 +0800

    新建knowledgeDisplay moudle

---
+++ knowledgedisplay/src/main/java/com/example/knowledgedisplay/KnowledgeDisplayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.knowledgedisplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.base.BaseActivity
+
+class KnowledgeDisplayActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_knowledge_display)
+    }
+}
